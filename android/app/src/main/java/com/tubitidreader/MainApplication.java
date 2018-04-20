46f6ad0e98a720672ce8074965cadcb404a7f005
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-04-21 01:17:59 +0300

    add vector icons to be used in the project.

--- android/app/src/main/java/com/tubitidreader/MainApplication.java
+++ android/app/src/main/java/com/tubitidreader/MainApplication.java
@@ -3,6 +3,8 @@ package com.tubitidreader;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNI18nPackage(),
             new RNCameraPackage()
       );
     }
