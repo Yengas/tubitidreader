a16d846ff51bee79e720883993f929979568baa1
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-04-21 14:10:24 +0300

    react-native-dialogs links for android.

--- android/app/src/main/java/com/tubitidreader/MainApplication.java
+++ android/app/src/main/java/com/tubitidreader/MainApplication.java
@@ -3,6 +3,7 @@ package com.tubitidreader;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDialogsPackage(),
             new VectorIconsPackage(),
             new RNI18nPackage(),
             new RNCameraPackage()
